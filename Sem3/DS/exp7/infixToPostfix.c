dcf6eb1d2cdb17682a5f8fcfb6c4a1937fb64a65
ChaitanyaCodes <66198526+ChaitanyaCodes@users.noreply.github.com>
2021-11-22 12:49:58 +0530

    add: print-7

--- Sem3/DS/exp7/infixToPostfix.c
+++ Sem3/DS/exp7/infixToPostfix.c
@@ -1,88 +1,74 @@
-//Stack Data Structure
-#include <stdio.h>
+#include<stdio.h>
+#include<ctype.h>
 #define SIZE_STACK 20
-int TOP = 0; //Pointer to the STACK
 int STACK[SIZE_STACK];
+int TOP = 0;
 
-int isSTACKFull()
-{
+int isSTACKFull(){
     if (TOP == SIZE_STACK)
         return 1;
     return 0;
 }
 
-void push(int val)
-{
-    if (isSTACKFull())
-    {
+void push(int item){
+    if (isSTACKFull()){
         printf("Stack is Full. \n");
         return;
     }
-    STACK[TOP++] = val;
-    //TOP++;
+    STACK[TOP++] = item;
 }
 
-int isSTACKEmpty()
-{
+int isSTACKEmpty(){
     if (TOP == 0)
         return 1;
     return 0;
 }
 
-int pop()
-{
-    if (isSTACKEmpty())
-    {
+int pop(){
+    if (isSTACKEmpty()){
         printf("Stack is Empty. \n");
         return -1;
     }
     return STACK[--TOP];
 }
 
-int getTopStack()
-{
+int getTopStack(){
     if (isSTACKEmpty())
-        return -1; //STACK EMPTY
+        return -1; // if stack is empty.
     return STACK[TOP - 1];
 }
 
-int getPrecedenceOfOperator(char c, int on_stack)
-{
-    switch (c)
-    {
-    case '+':
-    case '-':
-        return 1;
-    case '*':
-    case '/':
-        return 2;
-    case '^':
-        if (on_stack)
-            return 9;
-        else
-            return 10;
-    case '(':
-        if (on_stack)
-            return 0;
-        else
-            return 20;
-    default:
-        return -1; //STACK EMPTY
+int getPrecedenceOfOperator(char c, int on_stack){
+    switch (c){
+	    case '+':
+	    case '-':
+	        return 1;
+	    case '*':
+	    case '/':
+	        return 2;
+	    case '^':
+	        if (on_stack)
+	            return 9;
+	        else
+	            return 10;
+	    case '(':
+	        if (on_stack)
+	            return 0;
+	        else
+	            return 20;
+	    default:
+	        return -1; // if stack is empty.
     }
 }
 
-void inFixToPostFix(char s[])
-{
+void inFixToPostFix(char expre[]){
     int i = 0;
-    while (s[i] != '\0')
-    {
-        char x = s[i], tmp;
+    while (expre[i] != '\0'){
+        char x = expre[i], tmp;
         if ((x >= 65 && x <= 90) || (x >= 97 && x <= 122))
             printf("%c", x);
-        else
-        {
-            if (x == ')')
-            {
+        else{
+            if (x == ')'){
                 while ((tmp = pop()) != '(')
                     printf("%c", tmp);
                 i++;
@@ -101,8 +87,11 @@ void inFixToPostFix(char s[])
     printf("\n");
 }
 
-int main()
-{
-    inFixToPostFix("(a+b)*(c+d)");
+int main(){
+	char infixExpre[25];
+	printf("Enter the infix expression: ");
+	gets(infixExpre);
+	printf("Postfix expression: ");
+    inFixToPostFix(infixExpre);
     return 0;
 }
